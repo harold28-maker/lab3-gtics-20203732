82ca755a5447c5d34481b46fd85e8bc78f691fe8
Your Name <you@example.com>
2026-09-10 21:17:58 -0500

    second commit

---
+++ demo/src/main/java/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import repository.EmployeeRepository;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+    @GetMapping
+    public String listar(Model model){
+
+        
+    }
+
+}
